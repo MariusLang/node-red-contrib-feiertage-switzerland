a6b5d874ef2a49ee13f94cd4d727fb6ba6aa6eda
mariuslang <lang.marius@t-online.de>
2023-12-27 13:02:20 +0100

    isTomorrowHoliday function added

--- feiertage-switzerland.js
+++ feiertage-switzerland.js
@@ -1038,7 +1038,7 @@ module.exports = function (RED) {
     }
 
     function isTodayHoliday() {
-      // outputs boolean wether today is holiday
+      // outputs boolean whether today is holiday
       refreshHoliday(); // refresh holiday array
       let todayHoliday;
       if (holiday.length === 0) {
@@ -1053,8 +1053,31 @@ module.exports = function (RED) {
             todayHoliday = false;
           }
         }
-        node.send({ payload: todayHoliday });
       }
+      node.send({ payload: todayHoliday });
+    }
+
+    function isTomorrowHoliday() {
+      // outputs boolean whether tomorrow is holiday
+      refreshHoliday(); // refresh holiday array
+      let tomorrowHoliday = false;
+      if (holiday.length !== 0) {
+        const tomorrowDate = new Date();
+        tomorrowDate.setDate(tomorrowDate.getDate() + 1);
+        for (let i = 0; i < holiday.length; i += 1) {
+          const element = holiday[i];
+          const holidayDate = new Date(element.dateObj);
+          if (
+            holidayDate.getFullYear() === tomorrowDate.getFullYear()
+            && holidayDate.getMonth() === tomorrowDate.getMonth()
+            && holidayDate.getDate() === tomorrowDate.getDate()
+          ) {
+            tomorrowHoliday = true;
+            break;
+          }
+        }
+      }
+      node.send({ payload: tomorrowHoliday });
     }
 
     function sendNextHoliday() {
@@ -1091,16 +1114,14 @@ module.exports = function (RED) {
     }
 
     function isChristmasTime() {
-      if (new Date(Feiertage.getAdvent1(
-        Feiertage.formatDateObj, currentYear,
-      )).valueOf() <= new Date().valueOf()
+      if (new Date(Feiertage.getAdvent1(Feiertage.formatDateObj, currentYear)).valueOf()
+        <= new Date().valueOf()
         && new Date(Feiertage.getSilvester(
           Feiertage.formatDateObj, currentYear,
         )).valueOf() >= new Date().valueOf()) {
         node.send({ payload: true });
-      } else if (new Date(Feiertage.getNeujahr(
-        Feiertage.formatDateObj, currentYear,
-      )).valueOf() <= new Date().valueOf()
+      } else if (new Date(Feiertage.getNeujahr(Feiertage.formatDateObj, currentYear)).valueOf()
+        <= new Date().valueOf()
         && new Date(Feiertage.getHeiligeDreiKoenige(
           Feiertage.formatDateObj, currentYear,
         )).valueOf() >= new Date().valueOf()) {
@@ -1126,6 +1147,9 @@ module.exports = function (RED) {
           break;
         case 'isTodayHoliday':
           isTodayHoliday();
+          break;
+        case 'isTomorrowHoliday':
+          isTomorrowHoliday();
           break;
         case 'nextHoliday':
           sendNextHoliday();
@@ -1159,5 +1183,6 @@ module.exports = function (RED) {
       clearInterval(dailyInterval);
     });
   }
+
   RED.nodes.registerType('feiertageCH', feiertageCH);
 };
